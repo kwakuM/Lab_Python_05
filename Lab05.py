8b654fa21123573f5e338a175fe8f9645dfdac45
Ananse <ananse@ananse-laptop.(none)>
2012-06-25 17:07:41 +0000

    answers

---
+++ Lab05.py
@@ -0,0 +1,24 @@
+a=input("enter a:")
+#print a
+def factorial(a):
+    afactor=1
+    for i in range(1,a+1):
+                afactor*=i
+    return afactor
+print factorial(a)
+
+
+print '-------------------------------------------------------------'
+def fibonacci(a):
+    fib=[1,1]
+    for i in fib:
+            fibo=i+i
+            fib.append(fibo)
+    return fib
+print fibonacci(a)
+
+
+print'-------------------------------------------------------------------0'
+
+        
+
